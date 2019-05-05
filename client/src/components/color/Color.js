984f5b63f6e8f02f8033750046e91849d71b7dbe
Carpe-Omnia <40481048+Carpe-Omnia@users.noreply.github.com>
2019-05-05 03:13:23 -0400

    Colors look better.

--- client/src/components/color/Color.js
+++ client/src/components/color/Color.js
@@ -5,27 +5,29 @@ class Color extends React.Component {
     var rgb_text = this.props.rgb.replace("rgb","")
     return(
       <div style={{backgroundColor: this.props.rgb}} className="color_span">
-        <h4 id="living-coral" style={{color: 'white'}}>
+        <h4 className="color_name" style={{color: 'white'}}>
           {this.props.text}
         </h4>
         <span className="rgb_text" style={{color: 'white'}}>{rgb_text}</span>
         <br/>
         <span style={{display: 'block'}}>
-        <span style={{backgroundColor: 'black', color: this.props.rgb}} className="contrast_1">
+        <span style={{backgroundColor: 'black', color: this.props.rgb}} className="contrast_1 contrast">
           Black
         </span>
-        <span style={{backgroundColor: this.props.rgb, color: 'black'}} className="contrast_2">
-          Contrast
+        <span style={{backgroundColor: this.props.rgb, color: 'black'}} className="contrast_2 contrast">
+          <span className="contrast_2_text">Contrast</span>
         </span>
         </span>
         <span style={{display: 'block'}}>
-          <span style={{backgroundColor: 'white', color: this.props.rgb}} className="contrast_1">
+          <span style={{backgroundColor: 'white', color: this.props.rgb}} className="contrast_3 contrast">
             White
           </span>
-          <span style={{backgroundColor: this.props.rgb, color: 'white'}} className="contrast_2">
+          <span style={{backgroundColor: this.props.rgb, color: 'white'}} className="contrast_4 contrast">
             Contrast
           </span>
         </span>
+        <div className="black_bar"></div>
+        <div className="white_bar"></div>
       </div>
     )
   }
--- client/src/components/color/ColorIndex.js
+++ client/src/components/color/ColorIndex.js
@@ -1,8 +1,14 @@
 import React from 'react' ;
 import Color from './Color' ;
+function makeColor(text, rgbstring){
+  return(
+    {text: text, rgb: `rgb(${rgbstring})`}
+  )
+}
 const colors = [
-  {text: 'Living Coral', rgb: "rgb(255, 111, 97)" },
   {text: 'Twitter Blue', rgb:"rgb(29, 161, 242)"},
+  {text: 'Twitch Purple', rgb: "rgb(100, 65, 164)"},
+  {text: 'Living Coral', rgb: "rgb(255, 111, 97)" },
   {text: 'Burnt Henna', rgb: "rgb(119, 57, 52)"},
   {text: 'Rose Dawn', rgb: "rgb(187, 132, 123)"},
   {text: 'Coral Sands', rgb: "rgb(229, 168, 135)"},
@@ -16,8 +22,16 @@ const colors = [
   {text: "Magenta Haze", rgb: "rgb(150, 63, 109)"},
   {text: "Papaya", rgb: "rgb(245, 159, 106)"},
   {text: "Radiant Yellow", rgb: "rgb(242, 157, 55)"},
-  {text: "Amberglow", rgb: "rgb(210, 119, 70)"}
-]
+  {text: "Amberglow", rgb: "rgb(210, 119, 70)"},
+  makeColor("Some Grey", "74, 78, 77"),
+  makeColor("Sea Blue", "14, 154, 167"),
+  makeColor("Clear Sky", "61, 164, 171"),
+  makeColor("Charlie Brown", "246, 205, 97"),
+  makeColor("Canteloupe", "254, 138, 113")
+] //keep the names short so they don't overflow into a second line
+  //vh or whatever you're using appears to be a function of device height, so it messes with the 2-column structure
+  //make the CSS responsive so it looks good on all devices.
+  //make pallettes (combinations of colors) as a feature?
 class ColorIndex extends React.Component {
   render(){
     return(
